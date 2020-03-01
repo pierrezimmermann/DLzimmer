26ff14bfff3a59dc266dbfc25ab54212d38a6a19
pierrezimmermann <pierre.zimmermann@student.ecp.fr>
2020-03-01 22:12:16 +0100

    data augmentation

--- processData.py
+++ processData.py
@@ -1,11 +1,11 @@
 import numpy as np
 import glob
-from PIL import Image
 import os
-from tensorflow.keras.utils import normalize
 import tensorflow as tf
 
 
+# loads the images from the ./data folder and returns an array with iamages
+# and another array with labels
 def load_images():
     images = []
     labels = []
@@ -21,23 +21,4 @@ def load_images():
                 images.append(np.array(image))
                 labels.append(k)
                 print(img)
-    return np.array(images), np.array(labels)
-
-
-def load_few_images():
-    images = []
-    labels = []
-    artists = os.listdir('data/best-artworks-of-all-time/images/images')
-    for k in range(len(artists)):
-        i = 0
-        for img in glob.glob('data/best-artworks-of-all-time/images/images/{}/*'.format(artists[k])):
-            if i < 30:
-                image = Image.open(img)
-                image = image.resize((35, 35))
-                if np.array(image).shape == (35, 35, 3):
-                    images.append(np.array(image))
-                    labels.append(k)
-                    print(img)
-                image.close()
-                i += 1
     return np.array(images), np.array(labels)
--- train_2.py
+++ train_2.py
@@ -12,13 +12,18 @@ from model import CNN
 from processData import *
 from sklearn.utils import class_weight
 from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+
+# Load images and labels
 images, labels = load_images()
 p = np.random.permutation(range(len(images)))
 images, labels = images[p], labels[p]
 NUM_TEST = 800
 NUM_VAL = 800
 
+
+# Separate data into train, val and test datasets
 testX, testY = images[0:NUM_TEST].copy(), labels[0:NUM_TEST].copy()
 valX, valY = images[NUM_TEST:NUM_VAL +
                     NUM_TEST].copy(), labels[NUM_TEST:NUM_VAL+NUM_TEST].copy()
@@ -35,11 +40,13 @@ class_weight = class_weight.compute_class_weight(
 
 BATCH_SIZE = 64
 learning_rate = 0.005
+nb_epochs = 400
+dropout_rate = 0.7
 
 optimizer = tf.keras.optimizers.Adamax(learning_rate)
 loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
 
-dropout_rate = 0.7
+
 model = CNN(dropout_rate=dropout_rate)
 model.compile(optimizer=optimizer, loss=loss_object,
               metrics=['sparse_categorical_accuracy'])
@@ -47,10 +54,43 @@ model.compile(optimizer=optimizer, loss=loss_object,
 
 # Use early stopping to avoid overfitting
 callback = EarlyStopping(monitor='val_loss', min_delta=1e-4, patience=10)
-model.fit(trainX, trainY, batch_size=BATCH_SIZE,
-          epochs=400, validation_data=(valX, valY), class_weight=class_weight)
+
+
+# Train the model on the original dataset
+def train():
+    model.fit(trainX, trainY, batch_size=BATCH_SIZE,
+              epochs=400, validation_data=(valX, valY), class_weight=class_weight, callbacks=[callback])
+
+
+# Train on augmented data
+def train_with_data_augmentation():
+    datagen = ImageDataGenerator(
+        featurewise_center=True,
+        featurewise_std_normalization=True,
+        rotation_range=20,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        horizontal_flip=True)
+    for e in range(50):
+        print('Epochs {}'.format(e))
+        batches = 0
+        for x_batch, y_batch in datagen.flow(trainX, trainY, batch_size=BATCH_SIZE):
+            model.fit(x_batch, y_batch, class_weight=class_weight, verbose=0)
+            batches += 1
+            if batches >= len(trainX) / BATCH_SIZE:
+                # we need to break the loop by hand because
+                # the generator loops indefinitely
+                break
+    gty = model.call(valX)
+    perf = 100*np.mean(testY == tf.math.argmax(gty, 1))
+    print('Val perf: {}'.format(perf))
 
 
 # Evaluate perf on the test dataset
-gty = model.call(testX)
-perf = 100*np.mean(testY == tf.math.argmax(gty, 1))
+train()
+
+
+def test():
+    gty = model.call(testX)
+    perf = 100*np.mean(testY == tf.math.argmax(gty, 1))
+    print('Test perf : {}'.format(perf))
